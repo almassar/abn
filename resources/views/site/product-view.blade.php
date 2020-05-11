cae4f332deb173a4ba0de2934c14bb85876c7d13
Federer <almassar@mail.ru>
2020-05-11 22:41:10 +0600

    upd

--- resources/views/site/product-view.blade.php
+++ resources/views/site/product-view.blade.php
@@ -7,7 +7,7 @@
 
     <div style='overflow:hidden;'>
     @if(Storage::exists('public/products/'.$product->id.'.jpg'))
-        <img src="{{ asset('storage/products/'.$product->id.'.jpg') }}" alt="" style="max-width: 400px; float:left; margin-right:10px;">
+        <img src="{{ asset('storage/products/'.$product->id.'.jpg') }}" alt="" style="border-radius: 6px; border:1px solid silver; max-width: 400px; float:left; margin-right:10px;">
     @endif
    
 
